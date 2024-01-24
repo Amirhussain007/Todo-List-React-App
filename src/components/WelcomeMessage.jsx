110f89840af22a68b09aa8e466aafa03f977daaf
Md Amir Hussain <mdamirhussain17@gmail.com>
2024-01-24 22:52:28 +0530

    initial commit

---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+import styles from "./WelcomeMessage.module.css"
+
+import React from 'react'
+
+const WelcomeMessage = () => {
+  return <p className={styles.welcome}>Enjoy Your Day</p>
+}
+
+export default WelcomeMessage
